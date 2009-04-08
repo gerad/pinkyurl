0cb03997e2debba4cc38f7b01645a655e05b5377
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-04-08 03:28:06 -0700

    instructions from the home page

--- pinkyurl.rb
+++ pinkyurl.rb
@@ -2,6 +2,10 @@ require 'rubygems'
 require 'sinatra'
 require 'digest/sha1'
 
+get '/' do
+  'go to <a href="/url/www.google.com">/url/www.google.com</a>'
+end
+
 get %r{/url/(.*)} do |url|
   file = '/tmp/pinkyurl/' + Digest::SHA1.hexdigest(url) + '.png'
 
