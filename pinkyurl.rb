23699b7d6a7cdad21a5364bf33206077a9327cee
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-04-08 23:57:28 -0700

    support cropping

--- pinkyurl.rb
+++ pinkyurl.rb
@@ -1,16 +1,34 @@
 require 'rubygems'
 require 'sinatra'
 require 'digest/sha1'
+require 'image_science'
 
 get '/' do
   'go to <a href="/url/www.google.com">/url/www.google.com</a>'
 end
 
-get %r{/url/(.*)} do |url|
-  file = '/tmp/pinkyurl/' + Digest::SHA1.hexdigest(url) + '.png'
+get %r{(/crop/(\d+))?/url/(.*)} do |x, crop, url|
+  base = '/tmp/pinkyurl/' + Digest::SHA1.hexdigest(url)
+  file =  base + '.png'
 
   unless File.exists?(file)
     `xvfb-run --server-args="-screen 0, 1024x768x24" CutyCapt --url=http://#{url} --out=#{file}`
+  end
+
+  if crop
+    ImageScience.with_image file do |img|
+      w, h = img.width, img.height
+      l, t, r, b = 0, 0, w, h
+
+      t, b = 0, w if h > w
+
+      img.with_crop l, t, r, b do |cropped|
+        cropped.thumbnail crop do |thumb|
+	  file = base + "_#{crop}.png"
+	  thumb.save file
+        end
+      end
+    end
   end
 
   send_file file
