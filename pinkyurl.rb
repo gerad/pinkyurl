a8c69672a387f3b82298e3d64949b70a881077eb
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-04-16 08:54:55 -0700

    use public correctly to cache files for apache

--- pinkyurl.rb
+++ pinkyurl.rb
@@ -8,26 +8,23 @@ get '/' do
 end
 
 get %r{(/crop/(\d+))?/url/(.*)} do |x, crop, url|
-  base = '/tmp/pinkyurl/' + Digest::SHA1.hexdigest(url)
-  file =  base + '.png'
+  file = "public/url/#{url}"
 
-  unless File.exists?(file)
-    `xvfb-run -a --server-args="-screen 0, 1024x768x24" CutyCapt --url=http://#{url} --out=#{file}`
-  end
+  FileUtils.mkdir_p File.dirname(file)
+  `xvfb-run -a --server-args="-screen 0, 1024x768x24" CutyCapt --url=http://#{url} --out=#{file}`
 
   if crop
-    file = base + "_#{crop}.png"
-    unless File.exists?(file)
-      ImageScience.with_image file do |img|
-        w, h = img.width, img.height
-        l, t, r, b = 0, 0, w, h
+    ImageScience.with_image file do |img|
+      w, h = img.width, img.height
+      l, t, r, b = 0, 0, w, h
 
-        t, b = 0, w if h > w
+      t, b = 0, w if h > w
 
-        img.with_crop l, t, r, b do |cropped|
-          cropped.thumbnail crop do |thumb|
-            thumb.save file
-          end
+      img.with_crop l, t, r, b do |cropped|
+        cropped.thumbnail crop do |thumb|
+          file = "public/crop/#{crop}/url/#{url}"
+          FileUtils.mkdir_p File.dirname(file)
+          thumb.save file
         end
       end
     end
