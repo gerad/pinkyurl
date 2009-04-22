e4afbf683c728536ca0af6f76f317a9043578418
Visnu Pitiyanuvath <visnup@pulp-ubuntu.(none)>
2009-04-21 23:26:39 -0700

    use a properly encoded url param and use the entire thing as the key to the cached copy initial support for callbacks on when a cached copy is ready

--- pinkyurl.rb
+++ pinkyurl.rb
@@ -1,35 +1,42 @@
+require 'open-uri'
+require 'cgi'
 require 'rubygems'
 require 'sinatra'
-require 'digest/sha1'
 require 'image_science'
 
 get '/' do
-  'go to <a href="/crop/200/url/www.google.com">/crop/200/url/www.google.com</a>'
+  url = CGI.escape 'http://www.google.com'
+  href = 'i?url=' + url + '&crop=200'
+  "go to <a href=\"#{href}\">#{href}</a>"
 end
 
-get %r{(/crop/(\d+))?/url/(.*)} do |x, crop, url|
-  file = "public/url/#{url}"
-  rurl = url + '?' + params.reject { |k, v| k == 'captures' }.map do |k, v|
-    "#{k}=#{v}"
-  end.join('&')
+get '/i' do
+  url = params[:url]
+  file = "public/cache/#{params[:crop]}/#{CGI.escape url}"
 
-  FileUtils.mkdir_p File.dirname(file)
-  `xvfb-run -a --server-args="-screen 0, 800x600x24" CutyCapt --delay=1000 --out-format=png --url=http://#{rurl} --out=#{file}`
+  unless File.exists?(file)
+    FileUtils.mkdir_p File.dirname(file)
+    `xvfb-run -a --server-args="-screen 0, 800x600x24" CutyCapt --delay=1000 --out-format=png --url=#{url} --out=#{file}`
 
-  if crop
-    ImageScience.with_image file do |img|
-      w, h = img.width, img.height
-      l, t, r, b = 0, 0, w, h
+    if params[:crop]
+      ImageScience.with_image file do |img|
+        w, h = img.width, img.height
+        l, t, r, b = 0, 0, w, h
 
-      t, b = 0, w if h > w
+        t, b = 0, w if h > w
 
-      img.with_crop l, t, r, b do |cropped|
-        cropped.thumbnail crop do |thumb|
-          file = "public/crop/#{crop}/url/#{url}"
-          FileUtils.mkdir_p File.dirname(file)
-          thumb.save file
+        img.with_crop l, t, r, b do |cropped|
+          cropped.thumbnail params[:crop] do |thumb|
+            thumb.save file
+          end
         end
       end
+    end
+  end
+
+  if params[:callback]
+    Thread.new do
+      open params[:callback]
     end
   end
 
