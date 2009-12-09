c38c4665c4b182b57da6438a03c2af94802c14ee
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-12-09 00:08:41 -0800

    add newrelic

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   config.gem 'image_science'
   config.gem 'memcache-client', :lib => 'memcache'
+  config.gem 'newrelic_rpm'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
