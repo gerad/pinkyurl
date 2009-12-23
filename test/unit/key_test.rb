c39ec2272af4aa0ef07b7b11c0b455bf37803675
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-12-22 18:08:16 -0800

    fix tests

--- test/unit/key_test.rb
+++ test/unit/key_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class KeyTest < ActiveSupport::TestCase
   test "keys are case insensitive" do
     k = Key.create
-    bad = k.value.downcase
-    assert_not_equal bad, k.value
+    bad = k.secret.downcase
+    assert_not_equal bad, k.secret
     assert_equal k, Key.from_param(bad)
   end
 end
