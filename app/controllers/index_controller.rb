8ae58e6ad3b617f8c4b95d4f13b8778e242d34c7
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-12-23 16:24:48 -0800

    re-use a cookie if one was created for you link to keys/show from the free button

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,11 @@ class IndexController < ApplicationController
   def index
     @example = 'http://' + %w/ google.com nytimes.com yahoo.com /.rand
     @merchant_id = GoogleCheckout[:merchant_id]
-    @key = person.keys.first  if person
+    @key =
+      if cookies['key']
+        Key.find_by_value(cookies['key'])
+      elsif person
+        person.keys.first
+      end
   end
 end
