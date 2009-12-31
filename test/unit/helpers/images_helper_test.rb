aabc2ce478f63698d97662ad44a3c550f9d25b4e
Visnu Pitiyanuvath <visnupx@gmail.com>
2009-12-31 13:12:41 -0800

    for some stupid reason, I need to unescape even if the test passes without

--- test/unit/helpers/images_helper_test.rb
+++ test/unit/helpers/images_helper_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class ImagesHelperTest < ActionView::TestCase
+  test "polaroids unescapes html" do
+    assert_equal ["/i?resize=200&url=http%3A%2F%2Flocalhost%3A3000%2Fi%3Furl%3Dfoo"], polaroids(Image.all)
+    assert_equal '["/i?resize=200&url=http%3A%2F%2Flocalhost%3A3000%2Fi%3Furl%3Dfoo"]', polaroids(Image.all).to_json
+  end
 end
