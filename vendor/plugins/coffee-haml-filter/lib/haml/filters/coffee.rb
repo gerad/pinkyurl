d674eea70d0fef568b48cefeb54175a2bdbef8d5
Gerad Suyderhoud <gerads@gmail.com>
2010-04-11 14:32:04 -0700

    Braid: Added mirror 'vendor/plugins/coffee-haml-filter' at 'dc078a5'

---
+++ vendor/plugins/coffee-haml-filter/lib/haml/filters/coffee.rb
@@ -0,0 +1,18 @@
+module Haml::Filters::Coffee
+  include Haml::Filters::Base
+
+  def render_with_options(text, options)
+    js = IO.popen('coffee -sc', 'r+') do |c|
+      c << text
+      c.close_write
+      c.read
+    end
+    <<END
+<script type="#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}">
+  //<![CDATA[
+    #{js}
+  //]]>
+</script>
+END
+  end
+end
