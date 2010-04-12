3ece25fcfaf702f2843fc60c3b993219bdb6814f
Gerad Suyderhoud <gerads@gmail.com>
2010-04-12 16:16:23 -0700

    remove the coffee-script dependency

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,6 @@ Rails::Initializer.run do |config|
   config.gem 'image_science'
   config.gem 'memcache-client', :lib => 'memcache'
   config.gem 'newrelic_rpm'
-  config.gem 'coffee-script'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
